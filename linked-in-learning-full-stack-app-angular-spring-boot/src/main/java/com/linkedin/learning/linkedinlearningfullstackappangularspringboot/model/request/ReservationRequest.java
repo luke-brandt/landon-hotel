3f86bd4ffd2921d912936c88190914334558f629
Luke Brandt <lbrandt@lottery.idaho.gov>
2024-02-14 16:52:46 -0700

    first commit

---
+++ linked-in-learning-full-stack-app-angular-spring-boot/src/main/java/com/linkedin/learning/linkedinlearningfullstackappangularspringboot/model/request/ReservationRequest.java
@@ -0,0 +1,55 @@
+package com.linkedin.learning.linkedinlearningfullstackappangularspringboot.model.request;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+public class ReservationRequest {
+
+    private Long id;
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate checkin;
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate checkout;
+
+    public ReservationRequest() {
+    }
+
+    public ReservationRequest(Long id, LocalDate checkin, LocalDate checkout) {
+        this.id = id;
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(LocalDate checkin) {
+        this.checkin = checkin;
+    }
+
+    public LocalDate getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(LocalDate checkout) {
+        this.checkout = checkout;
+    }
+}
---
+++ linked-in-learning-full-stack-app-angular-spring-boot/src/main/java/com/linkedin/learning/linkedinlearningfullstackappangularspringboot/model/response/ReservationResponse.java
@@ -0,0 +1,52 @@
+package com.linkedin.learning.linkedinlearningfullstackappangularspringboot.model.response;
+
+import com.linkedin.learning.linkedinlearningfullstackappangularspringboot.model.Links;
+
+public class ReservationResponse {
+    private Long id;
+    private Integer roomNumber;
+
+    private Integer price;
+
+    private Links links;
+
+    public ReservationResponse() {
+    }
+
+    public ReservationResponse(Integer roomNumber, Integer price) {
+        this.roomNumber = roomNumber;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Links getLinks() {
+        return links;
+    }
+
+    public void setLinks(Links links) {
+        this.links = links;
+    }
+}
